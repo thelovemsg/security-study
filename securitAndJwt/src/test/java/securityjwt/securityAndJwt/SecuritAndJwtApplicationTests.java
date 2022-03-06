c927108a82a2c9fab1c2a99fbabb41f13e6799d6
thelovemsg <thelovemsg@naver.com>
2022-03-07 00:25:24 +0900

    update study content

---
+++ securitAndJwt/src/test/java/securityjwt/securityAndJwt/SecuritAndJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package securityjwt.securityAndJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritAndJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- security-study/src/main/java/io/security/basicsecurity/SecurityController.java
+++ security-study/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -16,18 +16,4 @@ public class SecurityController {
         return "loginPage";
     }
 
-    @GetMapping("/user")
-    public String user(){
-        return "user";
-    }
-
-    @GetMapping("/admin/pay")
-    public String adminPay(){
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin(){
-        return "admin";
-    }
 }
