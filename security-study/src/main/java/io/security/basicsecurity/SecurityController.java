972ba0f11ee8e00a11767fd3837a336806f7a647
thelovemsg <thelovemsg@naver.com>
2022-03-07 17:50:03 +0900

    study update to csrf

--- security-study/src/main/java/io/security/basicsecurity/SecurityController.java
+++ security-study/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -5,15 +5,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
-
     @GetMapping("/")
     public String index(){
         return "home";
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage(){
-        return "loginPage";
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay(){
+        return "adminPay";
+    }
+
+    @GetMapping("/admin/**")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String denied(){
+        return "Access is denied";
     }
 
 }
